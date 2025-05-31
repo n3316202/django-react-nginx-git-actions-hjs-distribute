6153a4a0ae99be546df0bc01368c0b4b7a354121
heo <admin@admin.com>
2025-06-01 02:50:27 +0900

    commit

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+# dev_3
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # settings/config 기준
+print("베이스 디렉토리",BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
