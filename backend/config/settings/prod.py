5d3d7a4e42c10702510956904a3663120754365d
heo <admin@admin.com>
2025-06-01 22:59:24 +0900

    commit

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -17,11 +17,11 @@ print('이후',BASE_DIR)
 
 # 정적파일 URL 경로
 STATIC_URL = '/static/'
-
-
 # collectstatic 명령어로 모이는 폴더 (EC2 내 정적파일 모음 경로)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # (개발 중에만) 추가 정적파일 경로 지정 가능
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
