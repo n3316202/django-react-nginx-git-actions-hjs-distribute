008a9a738577b3d0afff589ea12fa44884a43123
heo <admin@admin.com>
2025-06-02 17:34:27 +0900

    commit

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -58,3 +58,14 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', '3306'),
+#     }
+# }
